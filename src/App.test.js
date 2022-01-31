ce8c4cfe1d488d248aca5d9e49d4892293d012e9
Dublin Anondson <stratophone2112@gmail.com>
2022-01-30 22:04:57 -0600

    fix: test name for App was incorrect.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search component', () => {
   render(<App />);
   const linkElement = screen.getByText('Search Github Users');
   expect(linkElement).toBeInTheDocument();
