bad4bb0ab4d1d92a42dd55b102b597a15f29ca3d
Dublin Anondson <stratophone2112@gmail.com>
2022-01-30 18:23:53 -0600

    style: restyle the input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import './Input.css';
 
 function Input({
-  placeholder, onChange, disabled,
+  placeholder,
+  onChange,
+  disabled,
 }) {
   return (
     <input
+      className="input"
       disabled={disabled}
       placeholder={placeholder}
       onChange={onChange}
