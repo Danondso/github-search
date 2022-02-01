952abe87fc2c76a30662b7f056ff190cc61f6300
Dublin Anondson <stratophone2112@gmail.com>
2022-01-31 22:16:58 -0600

    fix: import useState like an adult dev would.

--- src/features/search/UserDetails/UserDetails.js
+++ src/features/search/UserDetails/UserDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useState } from 'react/cjs/react.development';
 import propTypes from 'prop-types';
